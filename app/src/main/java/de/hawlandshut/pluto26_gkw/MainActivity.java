908b12a710c5ade513ca2bff3e18557a97bbf1c4
Prof-Greipl <dieter.greipl@gmail.com>
2025-11-22 18:58:12 +0100

    HOMEWORK: "9.7 PostActivity"

--- app/src/main/java/de/hawlandshut/pluto26_gkw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto26_gkw/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         Log.d(TAG, "onStart called");
-        Intent intent = new Intent(getApplication(), CreateAccountActivity.class);
+        Intent intent = new Intent(getApplication(), PostActivity.class);
         startActivity(intent);
         super.onStart();
     }
